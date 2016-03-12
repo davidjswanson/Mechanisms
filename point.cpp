77dc22d246fe94a6d5226331ad8d2aca2330dfc4
davidjswanson <davidjswanson@gmail.com>
2016-03-12 09:38:35 -0600

    Definition of Point Class
    Points are the most basically defined object.

--- point.cpp
+++ point.cpp
@@ -1,22 +1,33 @@
-#include <stdlib.h>
-#include <math.h>
-#include <complex>
+#include <string>
 
 using namespace std;
+
+#include "./Coordinate.cpp"
+
+/* Class Point
+*  
+*/
 
 class Point{
 	public:
 		Point();
-		int getpt();	// dummy
+		Point(string myName) {name=myName;}	
+		bool setName(string);				//Return Name of the Point
+		bool setInitCoord(coordinate);		//Return Initial Coordinate of Point	
+		bool setIsGround (bool);			//Set Grounded Property
+		bool setRelation
+		
+		string getName();					//Return Name of the Point
+		Coordinate getInitCoord();			//Return Initial Coordinate of Point	
+		bool getIsGround();					//Return Grounded Property
+		
 	private:
+		string name;	// Identifier of the Point
+		Coordinate pointCoord	//Position
 		bool ground;	// Is this link fixed to the coordinate system
 		bool coupler;	// Is this point connected to different points
 };
 
 Point::Point(){
 	
-}
-
-int Point::getpt(){
-	return 10;
 }
